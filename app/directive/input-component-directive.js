570d5e9a55cd3399bf530799c81abed0b0585026
pravin varma <pravindvarma@gmail.com>
2017-01-12 02:41:35 +0530

    Updated the code to create new directive

--- app/directive/input-component-directive.js
+++ app/directive/input-component-directive.js
@@ -1,28 +1,49 @@
-(function(){
-    
-'use strict';
-    
-angular.module('inputModule').
-directive('accountNumber', inputDirectiveFunction)
-.directive('mobileNumber', inputDirectiveFunction);
+(function () {
 
-function inputDirectiveFunction() {
-  return {
-  restrict:'E',  
-  templateUrl: '../directive-templates/input-component-template.html',
-   transclude:true,      
-    scope: {
-      fieldId: '@fieldId',
-      fieldLabel: '@label',
-      fieldLimit: '=maxLimit',
-      isFieldRequired: '@isRequired',
-      placeHolder: '@placeHolder',
-      fieldPattern: '@pattern',
-      emptyMessage: '@emptyMessage',
-      inValidMessage: '@invalidMessage',
-        name:'@name',
-        ngModel: '@model'
+    'use strict';
+
+    angular.module('inputModule').
+    directive('accountNumber', inputDirectiveFunction)
+    .directive('mobileNumber', inputDirectiveFunction)
+    .directive('inValidMessage', validationMessageFunction)
+    .directive('emptyMessage', validationMessageFunction);
+
+    function inputDirectiveFunction() {
+        return {
+            restrict: 'E',
+            templateUrl: '../directive-templates/input-component-template.html',
+            transclude: true,
+            scope: {
+                fieldId: '@fieldId',
+                fieldLabel: '@label',
+                fieldLimit: '=maxLimit',
+                isFieldRequired: '@isRequired',
+                placeHolder: '@placeHolder',
+                fieldPattern: '@pattern',
+                emptyMessage: '@emptyMessage',
+                inValidMessage: '@invalidMessage',
+                name: '@name',
+                ngModel: '@model'
+            }
+        };
     }
-  }
-}
+
+    function validationMessageFunction() {
+        return {
+            controller: 'inputComponentController',
+            controllerAs: 'vm',
+            link: function (scope, el, attr) {
+                if(el[0].tagName === 'ACCOUNT-NUMBER')
+                scope.invalidMessageAccount = attr.invalidMessage;
+                
+                if(el[0].tagName === 'MOBILE-NUMBER')
+                scope.invalidMessageMobile = attr.invalidMessage;
+
+            }
+
+        };
+
+
+    }
+
 })();
--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,29 @@ var gulp = require('gulp');
 var uglify = require("gulp-uglify");
 var rename = require('gulp-rename');
 var concat = require('gulp-concat');
+var jshint = require('gulp-jshint');
+var color = require('gulp-color');
+var del = require('del');
 
 var jsFile = './app/**/*.js';
 
 // task
 gulp.task('minify-js', function () {
+    console.log(color('****Starting to minify*****','BLUE'));
     gulp.src(jsFile) // path to your files
         .pipe(concat('scripts.js'))
         .pipe(rename('scripts.min.js'))
         .pipe(uglify())
         .pipe(gulp.dest('./app/dist'));
+});
+
+gulp.task('jshint', function() {
+  return gulp.src(jsFile)
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('clean', function() {
+    console.log(color('****Cleaning the dist folder*****','YELLOW'));
+    return del(['dist/*.js']);
 });
